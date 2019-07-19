9e7515bf30f0d2d10ea082fcbd5ca09cb4936411
Tarenja <tarenya@hotmail.com>
2019-07-19 14:29:36 +0200

    added readme text

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
   const words = ['3dhubs', 'marvin', 'print', 'filament', 'order', 'layer']
-  const random = Math.floor(Math.random() * 6)
+  const random = Math.floor(Math.random() * words.length)
   const selected = words[random]
 
   for (let i = 0; i < selected.length; i++) {
